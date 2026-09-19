45d72ffbd0da5d9be4b9dd0834f8d9ae65e701f9
apple <apple@urshitas-mac.local>
2026-09-19 16:52:46 +0530

    Set default sqlite url in schema.prisma to file:./dev.db to prevent missing env error on Render

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = 'file:./dev.db';
-}
-
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const dbUrl = process.env.DATABASE_URL || 'file:./dev.db';
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: dbUrl,
+    },
+  },
+});
+
 
